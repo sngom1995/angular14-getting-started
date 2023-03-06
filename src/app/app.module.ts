1223cc238d24fff21312ecd29093104c8d76b4b3
sngom1995 <ngomsamba75@gmail.com>
2023-03-06 22:09:51 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {ProductListComponent} from "./products/product-list.component";
+import {FormsModule} from "@angular/forms";
+import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
+import {StarComponent} from "./shared/star.component";
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    StarComponent,
+    ConvertToSpacesPipe,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from "@angular/core";
+import {IProduct} from "./product";
+import {ProductService} from "./product.service";
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit{
+  pgTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  private _listFilter: string = '';
+  constructor(private productService: ProductService) {
+
+
+  }
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
+  toggleImage(): void{
+    this.showImage = !this.showImage;
+  }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(this.listFilter));
+  }
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+    this.listFilter = 'cart';
+  }
+
+  onRatingClicked(message: string): void {
+    this.pgTitle = 'Product List: ' + message;
+  }
+}
